a978825c1c411fc7fcb2a980175c2246809204e5
Donnaya <donnayawang@gmail.com>
2021-08-18 20:49:37 +0700

    [EDIT] Navbar and Path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
 import "./App.css";
 
 import Logo from "./images/logo.png";
@@ -11,32 +11,30 @@ import MainRoom from "./components/Room/MainRoom";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <nav className="navbar navbar-expand">
-          <Link to={"/"} className="navbar-brand">
-            <img src={Logo} alt="Adorminis" />
-          </Link>
-          <div className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link to={"/login"} className="nav-link">
-                Login
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={"/register"} className="nav-link">
-                Register
-              </Link>
-            </li>
-          </div>
-        </nav>
+      <nav className="navbar navbar-expand">
+        <Link to={"/"} className="navbar-brand">
+          <img src={Logo} alt="Adorminis" />
+        </Link>
+        <div className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link to={"/login"} className="nav-link">
+              Login
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/register"} className="nav-link">
+              Register
+            </Link>
+          </li>
+        </div>
+      </nav>
 
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/allroom/:dormID" component={MainRoom} />
-        </Switch>
-      </Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/allroom/:dormID" component={MainRoom} />
+      </Switch>
     </div>
   );
 }
