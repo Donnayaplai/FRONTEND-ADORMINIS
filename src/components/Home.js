1f374d424633e4138a16c2aeed69aaa461ac1d2d
Donnaya <donnayawang@gmail.com>
2021-08-18 20:53:01 +0700

    [EDIT] Link to Register and Login

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
-  const buttonstyle = {
-    backgroundColor: "#C7E5F0",
-    color: "#000",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    marginLeft: "auto",
-    marginRight: "auto",
-    maxWidth: "300px",
-    width: "90%",
-    maxHeight: "50px",
-    height: "100%",
-    boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-    borderRadius: "4px",
-    cursor: "pointer",
-  };
-
   return (
     <div className="container-fluid" style={{ height: "250px" }}>
       <h1 className="header mt-4 text-center">Join with Adorminis today!</h1>
-      <a
-        href="/register"
-        className="btn mt-4"
-        role="button"
-        style={buttonstyle}
-      >
-        Join now
-      </a>
-      <a href="./login" className="btn mt-4" role="button" style={buttonstyle}>
-        Login
-      </a>
-      <div className="web-content"></div>
+      <div className="d-grid gap-2 col-4 mx-auto mt-3">
+        <Link to={"/register"} className="btn btn-outline-primary mt-2">
+          Join now
+        </Link>
+        <Link to={"/login"} className="btn btn-outline-primary mt-3">
+          Login
+        </Link>
+      </div>
     </div>
   );
 }
