05200e6459bc7069e29f95571934192e363f0543
Donnaya <donnayawang@gmail.com>
2021-08-22 14:49:49 +0700

    [EDIT] Reference components in MainRoom

--- src/components/Room/MainRoom.js
+++ src/components/Room/MainRoom.js
@@ -1,5 +1,5 @@
 import React from "react";
-import AllRoom from "./AllRoom";
+// import AllRoom from "./AllRoom";
 import RoomAmountCard from "./RoomAmountCard";
 import RoomTable from "./RoomTable";
 
