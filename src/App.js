08ef90679122c88fd595cf682b98775d260a0390
Donnaya <donnayawang@gmail.com>
2021-09-01 12:43:54 +0700

    [EDIT] Path and style

--- src/App.js
+++ src/App.js
@@ -1,51 +1,50 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import "./App.css";
+import { useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
 
-// import Logo from "./images/logo.png";
-import Register from "./components/RegisterLogin/Register";
-import Login from "./components/RegisterLogin/Login";
-import Home from "./components/Home";
-// import MainRoom from "./components/Room/MainRoom";
-// import NoCodeRoom from "./components/Room/NoCodeRoom";
-import Utility from "./components/Utility/Utility";
-import UtilitySummary from "./components/Utility/UtilitySummary";
-import DormSetting from "./components/Dorm/DormSetting";
-import NotFoundPage from "./components/Others/NotFoundPage";
-import { PublicNav } from "./components/Navbar/PublicNav";
+import Alert from './components/Alert';
+import Register from './components/RegisterLogin/Register';
+import Login from './components/RegisterLogin/Login';
+import Home from './components/Home';
+import Utility from './components/Utility/Utility';
+import UtilitySummary from './components/Utility/UtilitySummary';
+import DormSetting from './components/Dorm/DormSetting';
+import NotFound from './components/Others/NotFound';
+// import PublicNav from './components/Navbar/PublicNav';
+import Navbar from './components/Navbar/Navbar';
+import AllRoom from './components/Room/AllRoom';
+
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
   return (
-    <div className="App">
-      <PublicNav />
-      {/* <nav className="navbar navbar-expand">
-        <Link to={"/"} className="navbar-brand">
-          <img src={Logo} alt="Adorminis" />
-        </Link>
-        <div className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link to={"/utility"} className="nav-link">
-              Utilities
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/dormsetting"} className="nav-link">
-              DormSetting
-            </Link>
-          </li>
-        </div>
-      </nav> */}
+    <Provider store={store}>
+      <Navbar />
+      <Alert />
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
+        <Route path='/' exact component={Home} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+        <Route path='/allroom/:dormid' component={AllRoom} />
         {/* <Route path="/allroom" component={MainRoom} />
         <Route path="/addresident/nocode" component={NoCodeRoom} /> */}
-        <Route path="/utility" component={Utility} />
-        <Route path="/utilsummary" component={UtilitySummary} />
-        <Route path="/dormsetting" component={DormSetting} />
-        <Route path="*" component={NotFoundPage} />
+        <Route path='/utility' component={Utility} />
+        <Route path='/utilsummary' component={UtilitySummary} />
+        <Route path='/dormsetting' component={DormSetting} />
+        <Route path='*' component={NotFound} />
       </Switch>
-    </div>
+    </Provider>
   );
 }
 
