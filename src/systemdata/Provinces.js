5e3a49f451a3cc2a42531abb3a912103f59f6744
Donnaya <donnayawang@gmail.com>
2021-09-01 12:48:43 +0700

    [ADD] Data for roles and provinces

---
+++ src/systemdata/Provinces.js
@@ -0,0 +1,158 @@
+export const Provinces = [
+  { id: 1, label: 'กรุงเทพมหานคร' },
+  { id: 2, label: 'กระบี่' },
+  { id: 3, label: 'กาญจนบุรี' },
+  { id: 4, label: 'กาฬสินธุ์' },
+  { id: 5, label: 'กำแพงเพชร' },
+  { id: 6, label: 'ขอนแก่น' },
+  { id: 7, label: 'จันทบุรี' },
+  { id: 8, label: 'ฉะเชิงเทรา' },
+  { id: 9, label: 'ชลบุรี' },
+  { id: 10, label: 'ชัยนาท' },
+  { id: 11, label: 'ชัยภูมิ' },
+  { id: 12, label: 'ชุมพร' },
+  { id: 13, label: 'เชียงราย' },
+  { id: 14, label: 'เชียงใหม่' },
+  { id: 15, label: 'ตรัง' },
+  { id: 16, label: 'ตราด' },
+  { id: 17, label: 'ตาก' },
+  { id: 18, label: 'นครนายก' },
+  { id: 19, label: 'นครปฐม' },
+  { id: 20, label: 'นครพนม' },
+  { id: 21, label: 'นครราชสีมา' },
+  { id: 22, label: 'นครศรีธรรมราช' },
+  { id: 23, label: 'นครสวรรค์' },
+  { id: 24, label: 'นนทบุรี' },
+  { id: 25, label: 'นราธิวาส' },
+  { id: 26, label: 'น่าน' },
+  { id: 27, label: 'บึงกาฬ' },
+  { id: 28, label: 'บุรีรัมย์' },
+  { id: 29, label: 'ปทุมธานี' },
+  { id: 30, label: 'ประจวบคีรีขันธ์' },
+  { id: 31, label: 'ปราจีนบุรี' },
+  { id: 32, label: 'ปัตตานี' },
+  { id: 33, label: 'พระนครศรีอยุธยา' },
+  { id: 34, label: 'พังงา' },
+  { id: 35, label: 'พัทลุง' },
+  { id: 36, label: 'พิจิตร' },
+  { id: 37, label: 'พิษณุโลก' },
+  { id: 38, label: 'เพชรบุรี' },
+  { id: 39, label: 'เพชรบูรณ์' },
+  { id: 40, label: 'แพร่' },
+  { id: 41, label: 'พะเยา' },
+  { id: 42, label: 'ภูเก็ต' },
+  { id: 43, label: 'มหาสารคาม' },
+  { id: 44, label: 'มุกดาหาร' },
+  { id: 45, label: 'แม่ฮ่องสอน' },
+  { id: 46, label: 'ยะลา' },
+  { id: 47, label: 'ยโสธร' },
+  { id: 48, label: 'ร้อยเอ็ด' },
+  { id: 49, label: 'ระนอง' },
+  { id: 50, label: 'ระยอง' },
+  { id: 51, label: 'ราชบุรี' },
+  { id: 52, label: 'ลพบุรี' },
+  { id: 53, label: 'ลำปาง' },
+  { id: 54, label: 'ลำพูน' },
+  { id: 55, label: 'เลย' },
+  { id: 56, label: 'ศรีสะเกษ' },
+  { id: 57, label: 'สกลนคร' },
+  { id: 58, label: 'สงขลา' },
+  { id: 59, label: 'สตูล' },
+  { id: 60, label: 'สมุทรปราการ' },
+  { id: 61, label: 'สมุทรสงคราม' },
+  { id: 62, label: 'สมุทรสาคร' },
+  { id: 63, label: 'สระแก้ว' },
+  { id: 64, label: 'สระบุรี' },
+  { id: 65, label: 'สิงห์บุรี' },
+  { id: 66, label: 'สุโขทัย' },
+  { id: 67, label: 'สุพรรณบุรี' },
+  { id: 68, label: 'สุราษฎร์ธานี' },
+  { id: 69, label: 'สุรินทร์' },
+  { id: 70, label: 'หนองคาย' },
+  { id: 71, label: 'หนองบัวลำภู' },
+  { id: 72, label: 'อ่างทอง' },
+  { id: 73, label: 'อุดรธานี' },
+  { id: 74, label: 'อุทัยธานี' },
+  { id: 75, label: 'อุตรดิตถ์' },
+  { id: 76, label: 'อุบลราชธานี' },
+  { id: 77, label: 'อำนาจเจริญ' },
+];
+
+{
+  /* <option selected>เลือก...</option>
+                      <option>กรุงเทพมหานคร</option>
+                      <option>กระบี่</option>
+                      <option>กาญจนบุรี</option>
+                      <option>กาฬสินธุ์</option>
+                      <option>กำแพงเพชร</option>
+                      <option>ขอนแก่น</option>
+                      <option>จันทบุรี</option>
+                      <option>ฉะเชิงเทรา</option>
+                      <option>ชลบุรี</option>
+                      <option>ชัยภูมิ</option>
+                      <option>ชุมพร</option>
+                      <option>เชียงราย</option>
+                      <option>เชียงใหม่</option>
+                      <option>ตรัง</option>
+                      <option>ตราด</option>
+                      <option>ตาก</option>
+                      <option>นครนายก</option>
+                      <option>นครปฐม</option>
+                      <option>นครพนม</option>
+                      <option>นครราชสีมา</option>
+                      <option>นครศรีธรรมราช</option>
+                      <option>นครสวรรค์</option>
+                      <option>นนทบุรี</option>
+                      <option>นราธิวาส</option>
+                      <option>น่าน</option>
+                      <option>หนองคาย</option>
+                      <option>หนองบัวลำภู</option>
+                      <option>บุรีรัมย์</option>
+                      <option>ปทุมธานี</option>
+                      <option>ประจวบคีรีขันธ์</option>
+                      <option>ปราจีนบุรี</option>
+                      <option>ปัตตานี</option>
+                      <option>พระนครศรีอยุธยา</option>
+                      <option>พังงา</option>
+                      <option>พัทลุง</option>
+                      <option>พิจิตร</option>
+                      <option>พิษณุโลก</option>
+                      <option>เพชรบุรี</option>
+                      <option>เพชรบูรณ์</option>
+                      <option>แพร่</option>
+                      <option>พะเยา</option>
+                      <option>ภูเก็ต</option>
+                      <option>มหาสารคาม</option>
+                      <option>แม่ฮ่องสอน</option>
+                      <option>มุกดาหาร</option>
+                      <option>ยะลา</option>
+                      <option>ยโสธร</option>
+                      <option>ร้อยเอ็ด</option>
+                      <option>ระนอง</option>
+                      <option>ระยอง</option>
+                      <option>ราชบุรี</option>
+                      <option>ลพบุรี</option>
+                      <option>ลำปาง</option>
+                      <option>ลำพูน</option>
+                      <option>เลย</option>
+                      <option>ศรีสะเกษ</option>
+                      <option>สกลนคร</option>
+                      <option>สงขลา</option>
+                      <option>สตูล</option>
+                      <option>สมุทรปราการ</option>
+                      <option>สมุทรสงคราม</option>
+                      <option>สมุทรสาคร</option>
+                      <option>สระแก้ว</option>
+                      <option>สระบุรี</option>
+                      <option>สิงห์บุรี</option>
+                      <option>สุโขทัย</option>
+                      <option>สุพรรณบุรี</option>
+                      <option>สุราษฎร์ธานี</option>
+                      <option>สุรินทร์</option>
+                      <option>อ่างทอง</option>
+                      <option>อุดรธานี</option>
+                      <option>อุทัยธานี</option>
+                      <option>อุตรดิตถ์</option>
+                      <option>อุบลราชธานี</option>
+                      <option>อำนาจเจริญ</option> */
+}
---
+++ src/systemdata/Role.js
@@ -0,0 +1,5 @@
+export const Roles = [
+  { id: 0, label: 'ตำแหน่ง' },
+  { id: 1, label: 'ผู้เช่า' },
+  { id: 2, label: 'พนักงาน' },
+];
