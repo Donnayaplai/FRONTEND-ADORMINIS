308ea0e4f6a2f63dba8f36efadb35e2430169927
Donnaya <donnayawang@gmail.com>
2021-09-09 19:52:23 +0700

    [EDIT] Path and Route

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
-import Alert from './components/Alert';
+import Alert from './components/Others/Alert';
 import Register from './components/RegisterLogin/Register';
 import Login from './components/RegisterLogin/Login';
-import Home from './components/Home';
+import HomePage from './pages/HomePage';
 import Utility from './components/Utility/Utility';
 import UtilitySummary from './components/Utility/UtilitySummary';
 import DormSetting from './components/Dorm/DormSetting';
 import NotFound from './components/Others/NotFound';
-// import PublicNav from './components/Navbar/PublicNav';
-import Navbar from './components/Navbar/Navbar';
-import AllRoom from './components/Room/AllRoom';
+import Room from './pages/Room';
+// import AllRoom from './components/Room/AllRoom';
+// import Accordion from './components/Accordion/Accordion';
+import DormList from './pages/DormList';
 
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
-import { loadUser } from './actions/auth';
+import { loadUser } from './actions/userActions';
 import setAuthToken from './utils/setAuthToken';
+import Layout from './components/Layout/Layout';
+import FrequentlyAskQuestions from './pages/FAQ';
+import ContactUs from './pages/ContactUs';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -28,23 +32,25 @@ function App() {
   useEffect(() => {
     store.dispatch(loadUser());
   }, []);
+
   return (
     <Provider store={store}>
-      <Navbar />
-      <Alert />
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/login' component={Login} />
-        <Route path='/register' component={Register} />
-        {/* <Route path='/allroom' component={AllRoom} /> */}
-        <Route path='/allroom/:dormid/:buildingid' component={AllRoom} />
-        {/* <Route path="/allroom" component={MainRoom} />
-        <Route path="/addresident/nocode" component={NoCodeRoom} /> */}
-        <Route path='/utility' component={Utility} />
-        <Route path='/utilsummary' component={UtilitySummary} />
-        <Route path='/dormsetting' component={DormSetting} />
-        <Route path='*' component={NotFound} />
-      </Switch>
+      <Layout>
+        <Alert />
+        <Switch>
+          <Route path='/' exact component={HomePage} />
+          <Route path='/login' exact component={Login} />
+          <Route path='/register' exact component={Register} />
+          <Route path='/faq' exact component={FrequentlyAskQuestions} />
+          <Route path='contact-us' exact component={ContactUs} />
+          <Route path='/all-building/:dormid' component={DormList} />
+          <Route path='/all-room/:buildingid' component={Room} />
+          <Route path='/utility' component={Utility} />
+          <Route path='/utilsummary' component={UtilitySummary} />
+          <Route path='/dormsetting' component={DormSetting} />
+          <Route path='*' component={NotFound} />
+        </Switch>
+      </Layout>
     </Provider>
   );
 }
