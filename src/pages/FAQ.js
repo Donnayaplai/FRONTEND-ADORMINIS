48c80f46d9c7d518a46472811e26cee236a550dd
Donnaya <donnayawang@gmail.com>
2021-09-09 19:44:51 +0700

    [ADD] Profile, FAQ, Contact page

---
+++ src/pages/FAQ.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import Accordion from '../components/Accordion/Accordion';
+function FAQ() {
+  return (
+    <Fragment>
+      <Accordion />
+    </Fragment>
+  );
+}
+
+export default FAQ;
