32cb0dcf053421929d9d5f55253abbb7886b4618
Donnaya <donnayawang@gmail.com>
2021-09-09 19:41:48 +0700

    [ADD] Mock data

---
+++ src/systemdata/FAQ.js
@@ -0,0 +1,14 @@
+export const Data = [
+  {
+    question: 'ADORMINIS คืออะไร',
+    answer: 'A labracadabrador.',
+  },
+  {
+    question: 'ADORMINIS เหมาะกับใครบ้าง',
+    answer: 'Hill-arious.',
+  },
+  {
+    question: 'ทำไมถึงต้องเลือกใช้ ADORMINIS ',
+    answer: 'I Apollo-gize.',
+  },
+];
