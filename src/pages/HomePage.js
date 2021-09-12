f4f609ec7bd3a85dae2d4e61dd0fd497f1493143
Donnaya <donnayawang@gmail.com>
2021-09-12 19:49:07 +0700

    [EDIT] Homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,18 +3,15 @@ import { Link } from 'react-router-dom';
 
 function HomePage() {
   return (
-    <div
-      className='container-fluid'
-      style={{ height: '250px', backgroundColor: '#F2F4ED', marginTop: '0' }}
-    >
+    <div className='container mt-5'>
       <h2 className='header text-center pt-4 pb-3 fw-bold'>
         ร่วมรับประสบการณ์ดี ๆ กับ ADORMINIS วันนี้!
       </h2>
       <div className='d-grid gap-2 col-4 mx-auto mt-3'>
-        <Link to={'/register'} className='btn btn-outline-primary mt-2'>
+        <Link to={'/register'} className='btn btn-primary mt-2'>
           สร้างบัญชีใหม่
         </Link>
-        <Link to={'/login'} className='btn btn-outline-primary mt-3'>
+        <Link to={'/login'} className='btn btn-primary mt-3'>
           เข้าสู่ระบบ
         </Link>
       </div>
