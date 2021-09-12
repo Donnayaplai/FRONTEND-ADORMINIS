a63e7528e537746da8a78ab9815a6a4f88988afd
Donnaya <donnayawang@gmail.com>
2021-09-12 19:09:13 +0700

    [EDIT] Navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,55 +1,47 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
 import './Navbar.css';
+import logo from '../../images/logo.png';
 
 function Navbar() {
-  const [click, setClick] = useState(false);
-  const [navbar, setNavbar] = useState(false);
-
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
-
-  const changeBackground = () => {
-    if (window.scrollY >= 80) {
-      setNavbar(true);
-    } else {
-      setNavbar(false);
-    }
-  };
-
-  window.addEventListener('scroll', changeBackground);
-
   return (
-    <nav className={navbar ? 'navbar active' : 'navbar'}>
-      <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-        ADORMINIS
-        <i class='fab fa-firstdraft' />
-      </Link>
-      <div className='menu-icon' onClick={handleClick}>
-        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+    <nav className='navbar fixed-top navbar-expand-lg'>
+      <div className='container-fluid'>
+        <Link to='/' className='navbar-brand'>
+          ADORMINIS
+          <img src={logo} alt='logo' />
+        </Link>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarTogglerDemo02'
+          aria-controls='navbarTogglerDemo02'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div className='collapse navbar-collapse' id='navbarTogglerDemo02'>
+          <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+            <li className='nav-item'>
+              <Link to='/' className='nav-link' aria-current='page'>
+                หน้าหลัก
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/faq' className='nav-link' aria-current='page'>
+                คำถามที่พบบ่อย
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/contact-us' className='nav-link' aria-current='page'>
+                ติดต่อเรา
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-        <li className='nav-item'>
-          <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-            หน้าหลัก
-          </Link>
-        </li>
-        <li className='nav-item'>
-          <Link to='/faq' className='nav-links' onClick={closeMobileMenu}>
-            คำถามที่พบบ่อย
-          </Link>
-        </li>
-        <li className='nav-item'>
-          <Link
-            to='/contact-us'
-            className='nav-links'
-            onClick={closeMobileMenu}
-          >
-            ติดต่อเรา
-          </Link>
-        </li>
-      </ul>
     </nav>
   );
 }
