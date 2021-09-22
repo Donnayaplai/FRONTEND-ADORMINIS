a5a11e8192b8e067b5b260997ce7a11870cd7e0a
Donnaya <donnayawang@gmail.com>
2021-09-22 13:24:49 +0700

    [EDIT] Routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Alert from './components/Others/Alert';
 import NotFound from './components/Others/NotFound';
 import Register from './components/RegisterLogin/Register';
 import Login from './components/RegisterLogin/Login';
 import Utility from './components/Utility/Utility';
 import UtilitySummary from './components/Utility/UtilitySummary';
 import DormSetting from './components/Dorm/DormSetting';
-import BuildingList from './components/Room/BuildingList';
-import RoomData from './components/Room/RoomData';
+import BuildingList from './components/RoomStatus/BuildingList';
+import Room from './components/RoomStatus/Room';
 
 const Routes = () => {
   return (
     <>
-      <Alert />
       <Switch>
-        <Route path='/login' exact component={Login} />
-        <Route path='/register' exact component={Register} />
-        <Route path='/all-building/:dormid' component={BuildingList} />
-        <Route path='/all-room/:buildingid' component={RoomData} /> ก
-        <Route path='/utility' component={Utility} />
-        <Route path='/utilsummary' component={UtilitySummary} />
-        <Route path='/dormsetting' component={DormSetting} />
-        <Route path='*' component={NotFound} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/all-building/:dormid" component={BuildingList} />
+        <Route path="/all-room/:buildingid" component={Room} />
+        <Route path="/utility" component={Utility} />
+        <Route path="/utilsummary" component={UtilitySummary} />
+        <Route path="/dormsetting" component={DormSetting} />
+        <Route path="*" component={NotFound} />
       </Switch>
     </>
   );
