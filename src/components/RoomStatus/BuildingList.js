c731a250dc7bef8877d3dbdd20782d63b8906d1b
Donnaya <donnayawang@gmail.com>
2021-09-22 13:45:32 +0700

    [EDIT] Component name

--- src/components/RoomStatus/BuildingList.js
+++ src/components/RoomStatus/BuildingList.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { useParams } from 'react-router';
 import building from '../../assets/images/building.jpg';
 
-const DormList = () => {
+const BuildingList = () => {
   const { dormid } = useParams();
 
   const [dormList, setDormList] = useState([]);
@@ -13,45 +13,45 @@ const DormList = () => {
   useEffect(() => {
     axios
       .get(`http://localhost:3001/api/building/all/${dormid}`)
-      .then(res => {
+      .then((res) => {
         console.log(res.data);
         setDormList(res.data);
         setLoading(true);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <div className='container'>
-      <h1 className='text-center'>
-        อาคารทั้งหมด <i className='fas fa-building'></i>
+    <div className="container">
+      <h1 className="text-center">
+        อาคารทั้งหมด <i className="fas fa-building"></i>
       </h1>
-      <div className='row'>
+      <div className="row">
         {loading &&
-          dormList.map(list => (
+          dormList.map((list) => (
             <div
-              className='col-sm-6 col-lg-4 col-xl-3 mt-3'
+              className="col-sm-6 col-lg-4 col-xl-3 mt-3"
               key={list.BUILDINGID}
             >
-              <div className='card col-lg-4' style={{ width: '18rem' }}>
+              <div className="card col-lg-4" style={{ width: '18rem' }}>
                 <img
                   src={building}
-                  className='card-img mx-auto'
-                  alt='buildings'
+                  className="card-img mx-auto"
+                  alt="buildings"
                 />
-                <div className='card-body'>
-                  <h5 className='card-title text-center'>
-                    <span className='fw-bold'>ชื่ออาคาร: </span>
+                <div className="card-body">
+                  <h5 className="card-title text-center">
+                    <span className="fw-bold">ชื่ออาคาร: </span>
                     {list.BUILDINGNAME}
                   </h5>
                 </div>
-                <div className='card-body mx-auto'>
+                <div className="card-body mx-auto">
                   <Link
                     to={`/all-room/${list.BUILDINGID}`}
-                    className='btn btn-lg'
+                    className="btn btn-lg"
                     style={{ backgroundColor: '#A4DBEA' }}
                   >
                     ข้อมูลห้องทั้งหมด
@@ -65,4 +65,4 @@ const DormList = () => {
   );
 };
 
-export default DormList;
+export default BuildingList;
