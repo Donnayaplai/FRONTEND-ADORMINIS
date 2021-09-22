6f95370b34b56e301c59f8fc38d7cd0e03540cd2
Donnaya <donnayawang@gmail.com>
2021-09-22 13:24:30 +0700

    [EDIT] Move BuildingList file to RoomStatus folder

--- src/components/RoomStatus/BuildingList.js
+++ src/components/RoomStatus/BuildingList.js
@@ -2,18 +2,17 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router';
-import building from '../components/images/building.jpg';
+import building from '../../assets/images/building.jpg';
 
 const DormList = () => {
   const { dormid } = useParams();
-  const { buildingid } = useParams();
 
   const [dormList, setDormList] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     axios
-      .get(`http://localhost:3001/building/${dormid}`)
+      .get(`http://localhost:3001/api/building/all/${dormid}`)
       .then(res => {
         console.log(res.data);
         setDormList(res.data);
@@ -22,11 +21,12 @@ const DormList = () => {
       .catch(err => {
         console.log(err);
       });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div className='container'>
-      <h1 className='mt-3 text-center'>
+      <h1 className='text-center'>
         อาคารทั้งหมด <i className='fas fa-building'></i>
       </h1>
       <div className='row'>
@@ -37,7 +37,11 @@ const DormList = () => {
               key={list.BUILDINGID}
             >
               <div className='card col-lg-4' style={{ width: '18rem' }}>
-                <img src={building} className='card-img-top' alt='buildings' />
+                <img
+                  src={building}
+                  className='card-img mx-auto'
+                  alt='buildings'
+                />
                 <div className='card-body'>
                   <h5 className='card-title text-center'>
                     <span className='fw-bold'>ชื่ออาคาร: </span>
@@ -46,7 +50,7 @@ const DormList = () => {
                 </div>
                 <div className='card-body mx-auto'>
                   <Link
-                    to={`/all-room/${buildingid}`}
+                    to={`/all-room/${list.BUILDINGID}`}
                     className='btn btn-lg'
                     style={{ backgroundColor: '#A4DBEA' }}
                   >
