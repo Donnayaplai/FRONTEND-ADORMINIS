1818822519b528de9617ff8e0d3ce2b26ce712fb
Donnaya <donnayawang@gmail.com>
2021-09-23 21:14:11 +0700

    [EDIT] Search component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,36 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function Search(props) {
-  const { onSearch } = props;
-
-  const [searchText, setSearchText] = useState('');
-
-  const handleInput = e => {
-    const text = e.target.value;
-    setSearchText(text);
-  };
-
-  const handleEnterKeyPressed = e => {
-    if (e.key === 'Enter') {
-      onSearch(searchText);
-    }
-  };
   return (
-    <div className='col-xl-6 col-md-4 mb-3 mx-auto'>
-      <div className='input-group'>
+    <div className="col-xl-6 col-md-4 mb-3 mx-auto">
+      <div className="input-group">
         <input
-          type='text'
-          className='form-control'
-          aria-label='Search'
-          aria-describedby='search-addon'
-          placeholder='พิมพ์เพื่อค้นหา...'
+          type="text"
+          className="form-control"
+          aria-label="Search"
+          aria-describedby="search-addon"
+          placeholder="พิมพ์เพื่อค้นหา..."
           style={{
             border: '1px solid #9ABCDF',
           }}
-          value={searchText}
-          onChange={handleInput}
-          onKeyPress={handleEnterKeyPressed}
-          name='search'
+          value={props.searchText}
+          onChange={props.handleInput}
+          name="search"
         />
       </div>
     </div>
