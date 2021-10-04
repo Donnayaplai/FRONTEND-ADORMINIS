4cb0ac9a47c59fdcb9caa5002e5d61db344ce25a
Donnaya <donnayawang@gmail.com>
2021-10-04 19:22:33 +0700

    [ADD] Select Role page

---
+++ src/components/RegisterLogin/SelectRole.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const SelectRole = () => {
+  return (
+    <Container>
+      <h1>เลือกตำแหน่งของคุณ</h1>
+    </Container>
+  );
+};
+
+export default SelectRole;
