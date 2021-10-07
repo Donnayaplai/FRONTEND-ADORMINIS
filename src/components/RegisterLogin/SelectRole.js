84b02d175151627b5c09fd79a33116d7e2029d84
Donnaya <donnayawang@gmail.com>
2021-10-07 20:25:31 +0700

    [EDIT] Select Role for register

--- src/components/RegisterLogin/SelectRole.js
+++ src/components/RegisterLogin/SelectRole.js
@@ -1,11 +1,48 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import residentIcon from '../../assets/images/resident.png';
+import adminIcon from '../../assets/images/admin.png';
+import { Link } from 'react-router-dom';
 
 const SelectRole = () => {
   return (
-    <Container>
+    <>
       <h1>เลือกตำแหน่งของคุณ</h1>
-    </Container>
+      <Container>
+        <Row>
+          <Col>
+            <Card style={{ maxWidth: '18rem' }} className="mx-auto">
+              <Card.Img variant="top" src={adminIcon} />
+              <Card.Body>
+                <Card.Title></Card.Title>
+                <Card.Text>
+                  <div className="d-flex justify-content-center">
+                    <Link to="/admin/register">
+                      <Button id="btn-save">ผู้จัดการหอพัก</Button>
+                    </Link>
+                  </div>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ maxWidth: '18rem' }}>
+              <Card.Img variant="top" src={residentIcon} />
+              <Card.Body>
+                <Card.Title></Card.Title>
+                <Card.Text>
+                  <div className="d-flex justify-content-center">
+                    <Link to="/resident/register">
+                      <Button id="btn-save">ผู้เช่า</Button>
+                    </Link>
+                  </div>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
