097735feaeea9abb1f63df48199873e6899ca9d8
Donnaya <donnayawang@gmail.com>
2021-10-16 15:15:37 +0700

    [ADD] Navbar based on role

---
+++ src/components/Navbar/AdminNav.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Navbar.css';
+import logo from '../../assets/images/building-nav.png';
+
+const AdminNav = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h2 className="title">
+          <Link to="/" style={{ textDecoration: 'none', color: '#fff' }}>
+            adorminis <img src={logo} alt="ADORMINIS-ICON" />{' '}
+          </Link>
+        </h2>
+      </div>
+      <input type="checkbox" id="click" />
+      <label htmlFor="click" className="menu-btn">
+        <i className="fas fa-bars"></i>
+      </label>
+      <ul>
+        <li>
+          <Link to="/">ข้อมูลหอพัก</Link>
+        </li>
+        <li>
+          <Link to="/">ใบแจ้งหนี้</Link>
+        </li>
+        <li>
+          <Link to="/">แจ้งปัญหา</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default AdminNav;
---
+++ src/components/Navbar/ResidentNav.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Navbar.css';
+import logo from '../../assets/images/building-nav.png';
+
+const ResidentNav = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h2 className="title">
+          <Link to="/" style={{ textDecoration: 'none', color: '#fff' }}>
+            adorminis <img src={logo} alt="ADORMINIS-ICON" />{' '}
+          </Link>
+        </h2>
+      </div>
+      <input type="checkbox" id="click" />
+      <label htmlFor="click" className="menu-btn">
+        <i className="fas fa-bars"></i>
+      </label>
+      <ul>
+        <li>
+          <Link to="/">ข้อมูลหอพัก</Link>
+        </li>
+        <li>
+          <Link to="/resident/invoice">ใบแจ้งหนี้</Link>
+        </li>
+        <li>
+          <Link to="/resident/payment/status">สถานะการชำระเงิน</Link>
+        </li>
+        <li>
+          <Link to="/resident/complain">แจ้งปัญหา</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default ResidentNav;
