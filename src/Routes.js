3a3b4b40383c66d47c8086e26edea3d8d1008b1b
Donnaya <donnayawang@gmail.com>
2021-10-17 00:20:42 +0700

    [EDIT] Routes

--- src/Routes.js
+++ src/Routes.js
@@ -8,7 +8,6 @@ import SelectRole from './components/RegisterLogin/SelectRole';
 import Login from './components/RegisterLogin/Login';
 import Utility from './components/Utility/Utility';
 import UtilitySummary from './components/Utility/UtilitySummary';
-// import DormSetting from './components/Dorm/DormSetting';
 import DormitorySetting from './components/Dorm/DormitorySetting';
 import BuildingList from './components/RoomStatus/BuildingList';
 import Room from './components/RoomStatus/Room';
@@ -17,17 +16,19 @@ import Billing from './components/Resident/Invoice';
 import PaymentStatus from './components/Resident/PaymentStatus';
 import UpdateResInfo from './components/RoomStatus/UpdateResInfo';
 import AddResident from './components/RoomStatus/AddResident';
-// import CheckAccount from './components/RegisterLogin/CheckAccount';
 import CheckExistAccount from './components/RegisterLogin/CheckExistAccount';
 
 import DynamicForm from './components/Setting/DynamicForm';
 import Setting from './components/Dorm/Setting';
-const Routes = () => {
+const Routes = (props) => {
   return (
     <>
       <Switch>
         {/* Public routes */}
-        <Route path="/login" exact component={Login} />
+
+        <Route path="/login">
+          <Login setRoleId={props.setRoleId} />
+        </Route>
         <Route path="/role-selection" component={SelectRole} />
         <Route path="/admin/register" exact component={adminRegister} />
         <Route
@@ -36,7 +37,11 @@ const Routes = () => {
           component={CheckExistAccount}
         />
         <Route path="/form" exact component={DynamicForm} />
-        <Route path="/resident/register" exact component={residentRegister} />
+        <Route
+          path="/resident/register/:userid"
+          exact
+          component={residentRegister}
+        />
         {/* Admin routes */}
         <Route path="/all-building/:dormid" component={BuildingList} />
         <Route path="/all-room/:buildingid" component={Room} />
@@ -51,7 +56,10 @@ const Routes = () => {
         <Route path="/dorm-registration" component={DormitorySetting} />
         <Route path="/dorm-setting" component={Setting} />
         {/* Resident routes */}
-        <Route path="/resident/invoice" component={Billing} />
+
+        <Route path="/resident/invoice">
+          <Billing roleId={props.roleId} />
+        </Route>
         <Route path="/resident/payment/status" component={PaymentStatus} />
         <Route path="*" component={NotFound} />
       </Switch>
