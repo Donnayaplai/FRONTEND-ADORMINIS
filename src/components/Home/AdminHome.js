8fbbe7cab59f403da2129da5268e4fdd55715a44
Donnaya <donnayawang@gmail.com>
2021-10-22 00:20:57 +0700

    [EDIT] Edit admin homepage

--- src/components/Home/AdminHome.js
+++ src/components/Home/AdminHome.js
@@ -1,17 +1,17 @@
-import React, { useEffect } from 'react';
-import { useHistory } from 'react-router';
+import React, { useEffect } from 'react'
+import { useHistory } from 'react-router'
 const AdminHome = (props) => {
-  const history = useHistory();
+  const history = useHistory()
   useEffect(() => {
     if (props.roleId !== 1) {
-      history.push('/login');
+      history.push('/login')
     }
-  }, []);
+  }, [])
   return (
     <div>
       <h1> Admin Homepage</h1>
     </div>
-  );
-};
+  )
+}
 
-export default AdminHome;
+export default AdminHome
