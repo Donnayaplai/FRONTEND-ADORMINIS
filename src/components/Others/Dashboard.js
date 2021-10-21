daeb27c9efb74386db268bd69254b73c16757662
Donnaya <donnayawang@gmail.com>
2021-10-22 00:27:09 +0700

    [ADD] Dashboard

---
+++ src/components/Others/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Dashboard = () => {
+  return (
+    <Container>
+      <h1>แดชบอร์ด</h1>
+    </Container>
+  );
+};
+
+export default Dashboard;
