efcab5272bf76a6ae428d26cf61ff32fb44cb4ae
Donnaya <donnayawang@gmail.com>
2021-10-22 00:18:29 +0700

    [EDIT] Building list

--- src/components/RoomStatus/BuildingList.js
+++ src/components/RoomStatus/BuildingList.js
@@ -1,19 +1,20 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { useParams } from 'react-router';
+// import { useParams } from 'react-router';
+import { Container, Button, Card, Row, Col } from 'react-bootstrap';
 import building from '../../assets/images/building.jpg';
 import env from '../../env';
-
+import './BuildingList.css';
 const BuildingList = (props) => {
-  const { dormid } = useParams();
+  // const { dormid } = useParams();
 
   const [dormList, setDormList] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     axios
-      .get(`${env.url}api/building/all/${dormid}`) //ส่ง dormid ที่ได้จาก userdetail /App มา
+      .get(`${env.url}api/building/all/${props.dormId}`) //ส่ง dormid ที่ได้จาก userdetail /App มา
       .then((res) => {
         console.log(res.data);
         setDormList(res.data);
@@ -22,47 +23,101 @@ const BuildingList = (props) => {
       .catch((err) => {
         console.log(err);
       });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  // useEffect(() => {
+  //   axios
+  //     .get(`${env.url}api/building/all/${dormid}`) //ส่ง dormid ที่ได้จาก userdetail /App มา
+  //     .then((res) => {
+  //       console.log(res.data);
+  //       setDormList(res.data);
+  //       setLoading(true);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
+
   return (
-    <div className="container">
-      <h1>
-        อาคารทั้งหมด <i className="fas fa-building"></i>
-      </h1>
-      <div className="row">
-        {loading &&
-          dormList.map((list) => (
-            <div
-              className="col-sm-6 col-lg-6 col-xl-3 mt-3 mx-auto justify-conter-center"
-              key={list.BUILDINGID}
-            >
-              <div className="card col-lg-4" style={{ width: '18rem' }}>
-                <img
-                  src={building}
-                  className="card-img mx-auto"
-                  alt="buildings"
-                />
-                <div className="card-body">
-                  <h5 className="card-title text-center">
-                    <span className="fw-bold">ชื่ออาคาร: </span>
-                    {list.BUILDINGNAME}
-                  </h5>
-                </div>
-                <div className="card-body mx-auto">
-                  <Link
-                    to={`/all-room/${list.BUILDINGID}`}
-                    className="btn btn-lg"
-                    style={{ backgroundColor: '#A4DBEA' }}
-                  >
-                    ข้อมูลห้องทั้งหมด
-                  </Link>
-                </div>
-              </div>
-            </div>
-          ))}
-      </div>
-    </div>
+    <>
+      <Container>
+        <h1>
+          อาคารทั้งหมด <i className="fas fa-building"></i>
+        </h1>
+        <Row>
+          {loading &&
+            dormList.map((list) => (
+              <Col>
+                <Card
+                  key={list.BUILDINGID}
+                  className="card-building"
+                  xs={10}
+                  sm={10}
+                  md={10}
+                >
+                  <Card.Img
+                    src={building}
+                    className="card-img"
+                    alt="building"
+                  />
+                  <Card.Body>
+                    <Card.Title>
+                      <h5>
+                        <span>ชื่ออาคาร: </span>
+                        {list.BUILDINGNAME}
+                      </h5>
+                    </Card.Title>
+                    <Card.Text>
+                      <Link to={`/all-room/${list.BUILDINGID}`}>
+                        <Button id="building-info">ข้อมูลห้องทั้งหมด</Button>
+                      </Link>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+        </Row>
+      </Container>
+      ;
+    </>
+    // <div className="container">
+    //   <h1>
+    //     อาคารทั้งหมด <i className="fas fa-building"></i>
+    //   </h1>
+
+    //   <div className="row">
+    //     {loading &&
+    //       dormList.map((list) => (
+    //         <div
+    //           className="col-sm-6 col-lg-6 col-xl-3 mt-3 mx-auto justify-conter-center"
+    //           key={list.BUILDINGID}
+    //         >
+    //           <div className="card col-lg-4" style={{ width: '18rem' }}>
+    //             <img
+    //               src={building}
+    //               className="card-img mx-auto"
+    //               alt="buildings"
+    //             />
+    //             <div className="card-body">
+    //               <h5 className="card-title text-center">
+    //                 <span className="fw-bold">ชื่ออาคาร: </span>
+    //                 {list.BUILDINGNAME}
+    //               </h5>
+    //             </div>
+    //             <div className="card-body mx-auto">
+    //               <Link
+    //                 to={`/all-room/${list.BUILDINGID}`}
+    //                 className="btn btn-lg"
+    //                 style={{ backgroundColor: '#A4DBEA' }}
+    //               >
+    //                 ข้อมูลห้องทั้งหมด
+    //               </Link>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       ))}
+    //   </div>
+    // </div>
   );
 };
 
