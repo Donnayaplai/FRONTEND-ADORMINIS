2994f2315f54603aae20183c4a80158584b1420d
Donnaya <donnayawang@gmail.com>
2021-10-22 12:57:00 +0700

    [ADD] Rent History

---
+++ src/components/History/RentHistory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const RentHistory = () => {
+  return (
+    <Container>
+      <h1>ประวัติการเช่าพัก</h1>
+    </Container>
+  );
+};
+
+export default RentHistory;
