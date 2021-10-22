2f52a38650401a9b85a44e51ffdecb2801de9ddb
Donnaya <donnayawang@gmail.com>
2021-10-22 12:56:24 +0700

    [ADD] Invoice components
    ใบแจ้งหนี้

---
+++ src/components/Invoice/InvoiceDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InvoiceDetail = () => {
+  return (
+    <Container>
+      <h1>ใบแจ้งหนี้ห้อง: </h1>
+    </Container>
+  );
+};
+
+export default InvoiceDetail;
---
+++ src/components/Invoice/InvoiceList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const InvoiceList = () => {
+  return (
+    <Container>
+      <h1>ใบแจ้งหนี้ทั้งหมด</h1>
+    </Container>
+  );
+};
+
+export default InvoiceList;
