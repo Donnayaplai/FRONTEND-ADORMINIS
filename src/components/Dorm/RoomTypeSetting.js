1cd24589ce257bf06f2a47e44f743efefb432c4d
Donnaya <donnayawang@gmail.com>
2021-10-23 23:37:43 +0700

    [EDIT] Room type setting

--- src/components/Dorm/RoomTypeSetting.js
+++ src/components/Dorm/RoomTypeSetting.js
@@ -1,171 +1,122 @@
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
-import React, { useState } from "react";
-import RemoveUser from "../../assets/images/delete.png";
+import React from 'react';
+import { useForm, useFieldArray, Controller } from 'react-hook-form';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import Delete from '../../assets/images/delete.png';
+import axios from 'axios';
+import env from '../../env';
 
-const RoomType = () => {
-  const [type, setType] = useState([]);
+const RoomTypeSetting = () => {
+  const { control, handleSubmit, reset, register } = useForm({
+    defaultValues: {
+      roomTypes: [{ ROOMTYPEID: '', roomType: '', roomPrice: '' }],
+    },
+  });
 
-  const IsValid = () => {
-    if (type.length === 0) {
-      return true;
-    }
-    const Empty = type.some(
-      (item) => item.roomtype === "" || item.roomprice === ""
-    );
-
-    if (Empty) {
-      type.map((item, index) => {
-        const allPrev = [...type];
-
-        if (type[index].roomtype === "") {
-          allPrev[index].errors.roomtype = "Type of room is required";
-        }
-
-        if (type[index].roomprice === "") {
-          allPrev[index].errors.roomprice = "Price is required";
-        }
-        setType(allPrev);
-      });
-    }
-
-    return !Empty;
-  };
-
-  const handleAddType = (e) => {
-    e.preventDefault();
-    const inputState = {
-      roomtype: "",
-      roomprice: "",
-
-      errors: {
-        roomtype: null,
-        roomprice: null,
-      },
-    };
-
-    if (IsValid()) {
-      setType((prev) => [...prev, inputState]);
-    }
-  };
-
-  const onChangeType = (index, event) => {
-    event.preventDefault();
-    event.persist();
-
-    setType((prev) => {
-      return prev.map((item, i) => {
-        if (i !== index) {
-          return item;
-        }
-
-        return {
-          ...item,
-          [event.target.name]: event.target.value,
-
-          errors: {
-            ...item.errors,
-            [event.target.name]:
-              event.target.value.length > 0
-                ? null
-                : [event.target.name] + " Is required",
-          },
-        };
-      });
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'roomTypes',
+  });
+  const onSubmit = async (data) => {
+    let roomTypeSetting = await axios.post(`${env.url}testja`, {
+      arrayRoomTypes: data,
     });
-  };
-  const handleRemove = (e, index) => {
-    e.preventDefault();
+    console.log(roomTypeSetting);
 
-    setType((prev) => prev.filter((item) => item !== prev[index]));
+    reset();
   };
+  // const onSubmit = async (data) => {
+  //   let roomTypeSetting = await axios.post(
+  //     `${env.url}setting/getRoomTypes/:dormID`,
+  //     {
+  //       roomTypes: data,
+  //     }
+  //   );
+
+  //   reset();
+  // };
 
   return (
-    <Container>
-      <Form>
+    <>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Container className="w-75">
           <h3>ตั้งค่าประเภทห้อง</h3>
           <Container
             className="py-4 rounded mb-3"
-            style={{ backgroundColor: "#EAE7E2" }}
+            style={{ backgroundColor: '#EAE7E2' }}
           >
-            {type.map((item, index) => (
-              <Container key={`item-${index}`}>
-                <Row className="mb-3">
+            {fields.map((item, index) => {
+              return (
+                <Row className="mb-3" key={item.id}>
                   <Col xl={4} md={4} sm={4} xs={4}>
-                    <Form.Group className="mb-3">
-                      <Form.Label>ประเภทห้องพัก</Form.Label>
-                      <Form.Control
-                        className={
-                          item.errors.roomtype
-                            ? "form-control  is-invalid"
-                            : "form-control"
-                        }
-                        name="roomtype"
-                        id="roomtype"
-                        value={item.roomtype}
-                        onChange={(e) => onChangeType(index, e)}
-                        type="text"
-                        placeholder="ห้องชั้นบน"
-                      />
-                    </Form.Group>
+                    <Form.Label>ประเภทห้องพัก</Form.Label>
+                    {/* <input {...register(`roomTypes.${index}.roomType`)} /> */}
+                    <Controller
+                      control={control}
+                      name={`roomTypes.${index}.roomType`}
+                      render={({ field }) => <input {...field} />}
+                    />
                   </Col>
                   <Col xl={4} md={4} sm={4} xs={4}>
                     <Form.Group className="mb-3">
                       <Form.Label>ราคา</Form.Label>
-                      <Form.Control
+                      {/* <input {...register(`roomTypes.${index}.roomPrice`)} /> */}
+                      <Controller
+                        control={control}
+                        className="form-input"
                         type="text"
-                        className={
-                          item.errors.roomprice
-                            ? "form-control  is-invalid"
-                            : "form-control"
-                        }
-                        name="roomprice"
-                        placeholder="4000"
-                        id="roomprice"
-                        value={item.roomprice}
-                        onChange={(e) => onChangeType(index, e)}
+                        name={`roomTypes.${index}.roomPrice`}
+                        defaultValue={item.roomPrice}
+                        render={({ field }) => <input {...field} />}
                       />
                     </Form.Group>
                   </Col>
 
                   <Col xl={4} md={4} sm={4} xs={4}>
                     <Button
+                      type="button"
                       style={{
-                        backgroundColor: "transparent",
-                        border: "none",
-                        boxShadow: "none",
+                        backgroundColor: 'transparent',
+                        border: 'none',
+                        boxShadow: 'none',
                       }}
-                      variant="danger"
-                      onClick={(e) => handleRemove(e, index)}
+                      onClick={() => remove(index)}
                     >
                       <img
-                        src={RemoveUser}
-                        alt="Remove resident"
+                        src={Delete}
+                        alt="Remove room type"
                         style={{ maxWidth: '2rem', marginTop: '1.5em' }}
                       />
                     </Button>
                   </Col>
                 </Row>
-              </Container>
-            ))}
-            <Button variant="light" onClick={handleAddType}>
-              เพิ่มประเภทห้องพัก
-            </Button>
+              );
+            })}
+            <Row>
+              <Col>
+                <Button
+                  type="button"
+                  variant="light"
+                  onClick={() => append({ roomType: '', roomPrice: '' })}
+                >
+                  เพิ่มประเภทห้องพัก
+                </Button>
+              </Col>
+            </Row>
           </Container>
-          <Row style={{ marginTop: "5%" }}>
+          <Row className="mt-3">
             <Col>
               <Button id="btn-save">ย้อนกลับ</Button>
             </Col>
             <Col>
-              <Button id="btn-save" type="submit" style={{ float: "right" }}>
+              <Button id="btn-save" type="submit" style={{ float: 'right' }}>
                 บันทึก
               </Button>
             </Col>
           </Row>
         </Container>
       </Form>
-    </Container>
+    </>
   );
 };
 
-export default RoomType;
+export default RoomTypeSetting;
