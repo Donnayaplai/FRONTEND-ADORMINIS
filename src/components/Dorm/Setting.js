9d1fffe2029ce7bc1a0c1b6b5809b4b7cdb58b04
Donnaya <donnayawang@gmail.com>
2021-10-28 07:30:15 +0700

    [EDIT] Setting and add CSS file

--- src/components/Dorm/Setting.js
+++ src/components/Dorm/Setting.js
@@ -1,23 +1,26 @@
 import React, { useState } from 'react';
 import { Container, Tab, Tabs } from 'react-bootstrap';
+import { Route, Switch } from 'react-router-dom';
+
 import CostSetting from './CostSetting';
 import BuildingSetting from './BuildingSetting';
 import RoomType from './RoomTypeSetting';
-import { Route, Switch } from 'react-router-dom';
+import CreateRoom from './CreateRoom';
 
 const Setting = () => {
-  const [key, setKey] = useState('builiding-setting');
+  const [key, setKey] = useState('cost-setting');
   <Switch>
     <Route path="/cost-setting" component={CostSetting} />
     <Route path="/building-setting" component={BuildingSetting} />
     <Route path="/roomtype-setting" component={RoomType} />
+    <Route path="/create-room" component={CreateRoom} />
   </Switch>;
   return (
     <>
       <h1>ตั้งค่าหอพัก</h1>
       <Container>
         <Tabs activeKey={key} onSelect={(k) => setKey(k)} className="mb-5">
-          <Tab eventKey="utility-setting" title="ค่าใช้จ่าย">
+          <Tab eventKey="cost-setting" title="ค่าใช้จ่าย">
             <CostSetting />
           </Tab>
           <Tab eventKey="builiding-setting" title="ตึก">
@@ -26,8 +29,8 @@ const Setting = () => {
           <Tab eventKey="roomtype-setting" title="ประเภทห้องพัก">
             <RoomType />
           </Tab>
-          <Tab eventKey="room-setting" title="สร้างห้องพัก">
-            <RoomType />
+          <Tab eventKey="create-room" title="สร้างห้องพัก">
+            <CreateRoom />
           </Tab>
         </Tabs>
       </Container>
