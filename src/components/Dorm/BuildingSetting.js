2203791128feecf3d4124eac2464fa4ecf819263
Donnaya <donnayawang@gmail.com>
2021-10-28 07:31:19 +0700

    [EDIT] Building setting

--- src/components/Dorm/BuildingSetting.js
+++ src/components/Dorm/BuildingSetting.js
@@ -1,190 +1,140 @@
+import React from 'react';
+import { useForm, useFieldArray, Controller } from 'react-hook-form';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
-import React, { useState } from 'react';
-import RemoveUser from '../../assets/images/delete.png';
+import axios from 'axios';
+import env from '../../env';
+import Edit from '../../assets/images/edit.png';
+import Delete from '../../assets/images/delete.png';
+import './Setting.css';
 
-const BuildingSetting = () => {
-  const [form, setForm] = useState([]);
+const BuildingSetting = (props) => {
+  const { control, handleSubmit, reset } = useForm({});
 
-  const prevIsValid = () => {
-    if (form.length === 0) {
-      return true;
-    }
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'arrayBuilding',
+  });
 
-    const someEmpty = form.some(
-      (item) => item.buildingName === '' || item.numOfFloor === ''
-    );
-
-    if (someEmpty) {
-      form.map((item, index) => {
-        const allPrev = [...form];
-
-        if (form[index].buildingName === '') {
-          allPrev[index].errors.buildingName = 'โปรดกรอกชื่อตึก';
-        }
-
-        if (form[index].Username === '') {
-          allPrev[index].errors.numOfFloor = 'โปรดกรอกจำนวนชั้น';
-        }
-        setForm(allPrev);
-      });
-    }
-    return !someEmpty;
-  };
-
-  const handleAddLink = (e) => {
-    e.preventDefault();
-    const inputState = {
-      buildingName: '',
-      numOfFloor: '',
-
-      errors: {
-        buildingName: null,
-        numOfFloor: null,
-      },
-    };
-
-    if (prevIsValid()) {
-      setForm((prev) => [...prev, inputState]);
-    }
-  };
-
-  const onChange = (index, event) => {
-    event.preventDefault();
-    event.persist();
-
-    setForm((prev) => {
-      return prev.map((item, i) => {
-        if (i !== index) {
-          return item;
-        }
-
-        return {
-          ...item,
-          [event.target.name]: event.target.value,
-
-          errors: {
-            ...item.errors,
-            [event.target.name]:
-              event.target.value.length > 0
-                ? null
-                : [event.target.name] + ' จำเป็นต้องกรอก',
-          },
-        };
-      });
+  const onSubmit = async (data) => {
+    let buildingSetting = await axios.post(`${env.url}testja`, {
+      arrayBuilding: data,
     });
-  };
+    console.log(buildingSetting);
 
-  const handleRemoveField = (e, index) => {
-    e.preventDefault();
-    setForm((prev) => prev.filter((item) => item !== prev[index]));
+    reset();
   };
+  // const onSubmit = async (data) => {
+  //   let buildingSetting = await axios.post(
+  //     `${env.url}setting/getRoomTypes/${props.dormId}`,
+  //     {
+  //       arrayBuilding: data,
+  //     }
+  //   );
+  //   console.log(buildingSetting);
 
-  // const settingBuilding = async (form) => {
-  //   form.preventDefault();
-  //   console.log(form);
+  //   reset();
   // };
 
   return (
     <>
-      <Container className="w-75">
-        <Form>
-          <h3>ตั้งค่าตึก</h3>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Container className="w-75">
+          <Row>
+            <Col xl={4} md={4} sm={8} xs={8}>
+              <h3>ตั้งค่าตึก</h3>
+            </Col>
+            <Col>
+              <img
+                src={Edit}
+                alt="Edit roomtype setting"
+                style={{ maxWidth: '2rem', float: 'right' }}
+              />
+            </Col>
+          </Row>
+
           <Container
             className="py-4 rounded mb-3"
             style={{ backgroundColor: '#EAE7E2' }}
           >
-            {form.map((item, index) => (
-              <Container key={`item-${index}`}>
-                <Row className="mb-3">
+            {fields.map((item, index) => {
+              return (
+                <Row className="mb-3" key={item.id}>
                   <Col xl={4} md={4} sm={4} xs={4}>
-                    <Form.Group className="mb-3">
-                      <Form.Label>ชื่อตึก</Form.Label>
-                      <Form.Control
-                        className={
-                          item.errors.buildingName
-                            ? 'form-control  is-invalid'
-                            : 'form-control'
-                        }
-                        name="buildingName"
-                        value={item.buildingName}
-                        onChange={(e) => onChange(index, e)}
-                        type="text"
-                        placeholder="กอไก่ 1"
-                      />
-                      {item.errors.buildingName && (
-                        <div className="invalid-feedback">
-                          {item.errors.buildingName}
-                        </div>
+                    <Form.Label>ชื่อตึก</Form.Label>
+                    <Controller
+                      control={control}
+                      name={`arrayBuilding.${index}.BUILDINGNAME`}
+                      render={({ field }) => (
+                        <Form.Control
+                          type="text"
+                          placeholder="ประเภทห้องพัก"
+                          {...field}
+                        />
                       )}
-                    </Form.Group>
+                    />
                   </Col>
                   <Col xl={4} md={4} sm={4} xs={4}>
                     <Form.Group className="mb-3">
                       <Form.Label>จำนวนชั้น</Form.Label>
-                      <Form.Control
-                        type="number"
-                        className={
-                          item.errors.numOfFloor
-                            ? 'form-control  is-invalid'
-                            : 'form-control'
-                        }
-                        name="numOfFloor"
-                        value={item.numOfFloor}
-                        onChange={(e) => onChange(index, e)}
-                        min="1"
+                      <Controller
+                        control={control}
+                        name={`arrayBuilding.${index}.NUMOFFLOOR`}
+                        defaultValue={item.NUMOFFLOOR}
+                        render={({ field }) => (
+                          <Form.Control
+                            type="number"
+                            placeholder="ราคา"
+                            min="0"
+                            {...field}
+                          />
+                        )}
                       />
-                      {item.errors.numOfFloor && (
-                        <div className="invalid-feedback">
-                          {item.errors.numOfFloor}
-                        </div>
-                      )}
                     </Form.Group>
                   </Col>
 
                   <Col xl={4} md={4} sm={4} xs={4}>
                     <Button
+                      type="button"
                       style={{
                         backgroundColor: 'transparent',
                         border: 'none',
                         boxShadow: 'none',
-                        width: 'fit-content',
                       }}
-                      onClick={(e) => handleRemoveField(e, index)}
+                      onClick={() => remove(index)}
                     >
                       <img
-                        src={RemoveUser}
-                        alt="Remove resident"
+                        src={Delete}
+                        alt="Remove room type"
                         style={{ maxWidth: '2rem', marginTop: '1.5em' }}
                       />
                     </Button>
                   </Col>
                 </Row>
-              </Container>
-            ))}
-            <Container>
-              <Button variant="light" onClick={handleAddLink}>
-                เพิ่มจำนวนตึก
-              </Button>
-            </Container>
+              );
+            })}
+            <Row>
+              <Col>
+                <Button
+                  type="button"
+                  id="button-add"
+                  onClick={() =>
+                    append({ BUILDINGID: '', BUILDINGNAME: '', NUMOFFLOOR: '' })
+                  }
+                >
+                  เพิ่มตึก
+                </Button>
+              </Col>
+            </Row>
           </Container>
-
-          <Row style={{ marginTop: '5%' }}>
+          <Row className="mt-3 mb-5">
             <Col>
-              <Button id="btn-save">ย้อนกลับ</Button>
-            </Col>
-            <Col>
-              <Button
-                id="btn-save"
-                type="submit"
-                style={{ float: 'right' }}
-                // onClick={console.log('hi')}
-              >
+              <Button id="btn-save" type="submit" style={{ float: 'right' }}>
                 บันทึก
               </Button>
             </Col>
           </Row>
-        </Form>
-      </Container>
+        </Container>
+      </Form>
     </>
   );
 };
