3e7b3d00b882f219faa553b64a75de4a25aa8001
Donnaya <donnayawang@gmail.com>
2021-10-29 14:32:53 +0700

    [EDIT] Dorm profile

--- src/components/Resident/DormProfile.js
+++ src/components/Resident/DormProfile.js
@@ -1,20 +1,48 @@
-import React from "react";
-import axios from "axios";
-import env from "../../env";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
-import { Provinces } from "../../systemdata/Provinces";
-import { useForm } from "react-hook-form";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import env from '../../env';
+import { Container, Row, Col } from 'react-bootstrap';
+// import { useHistory } from 'react-router';
 
-const DormProfile = () => {
+const DormProfile = (props) => {
+  // useEffect(() => {
+  //   if (props.roleId !== 0) {
+  //     window.alert('คุณไม่มีสิทธิ์ในการเข้าถึง โปรดเข้าสู่ระบบ');
+  //     history.push('/login');
+  //   }
+  // }, []);
+  const [dormData, setDormData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getDormProfile = async () => {
+      try {
+        const response = await axios.get(`${env.url}dorm/info/${props.dormId}`);
+        setDormData(response.data);
+        console.log(dormData);
+      } catch (error) {
+        console.error(error);
+      }
+      setLoading(false);
+    };
+
+    getDormProfile();
+  }, []);
+
+  if (loading) {
+    return <h2 className="text-center fs-3 mt-5">Loading...</h2>;
+  }
   return (
     <>
-      <h1>ข้อมูลหอพัก</h1>
+      <h1>
+        ข้อมูลหอพัก <i className="fas fa-info-circle"></i>
+      </h1>
 
-      <Container className="w-50">
+      <Container className="w-75">
         <h5 className="fw-bold">ข้อมูลและที่อยู่</h5>
         <Container
           className="p-3 rounded w-100 mb-3"
-          style={{ backgroundColor: "#EAE7E2" }}
+          style={{ backgroundColor: '#EAE7E2' }}
         >
           <Row className="mb-3">
             <Col xs={12} sm={12} md={6}>
