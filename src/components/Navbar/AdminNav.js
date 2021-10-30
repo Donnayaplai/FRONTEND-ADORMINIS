746e62b6a645f5f39e5700aac1a8bfb1364696ff
nattacha <natchaaer@gmail.com>
2021-10-30 14:59:00 +0700

    [EDIT] navbar

--- src/components/Navbar/AdminNav.js
+++ src/components/Navbar/AdminNav.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
-import { useHistory } from 'react-router';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, NavDropdown } from "react-bootstrap";
+import { useHistory } from "react-router";
 
-import './Navbar.css';
-import logo from '../../assets/images/building-nav.png';
+import "./Navbar.css";
+import logo from "../../assets/images/building-nav.png";
 
 const AdminNav = (props) => {
   const history = useHistory();
   const logout = () => {
-    localStorage.removeItem('authorization');
+    localStorage.removeItem("authorization");
     props.setRoleId(null);
-    history.push('/login');
+    history.push("/login");
   };
   return (
     <nav>
@@ -20,28 +20,32 @@ const AdminNav = (props) => {
           <Link
             to="/admin/home"
             style={{
-              textDecoration: 'none',
-              color: '#fff',
-              fontSize: '1.5rem',
+              textDecoration: "none",
+              color: "#fff",
+              fontSize: "1.5rem",
             }}
           >
             adorminis <img src={logo} alt="ADORMINIS-ICON" />
           </Link>
         </h2>
       </div>
-      <input type="checkbox" id="click" style={{ display: 'none' }} />
+      <input type="checkbox" id="click" style={{ display: "none" }} />
       <label htmlFor="click" className="menu-btn">
         <i className="fas fa-bars"></i>
       </label>
       <ul>
+        <NavDropdown title="หอพัก" id="navbarDropdown">
+          <NavDropdown.Item href="#action3">ข้อมูล</NavDropdown.Item>
+          <NavDropdown.Item href="#action3">ตั้งค่า</NavDropdown.Item>
+        </NavDropdown>
         <li>
           <Link to={`/all-building/${props.dormId}`}>ตึกและห้องพัก</Link>
         </li>
         <li>
-          <Link to={`/all-invoice/list/${props.dormId}`}>ใบแจ้งหนี้</Link>
+          <Link to="/select-building/meter-record">จดมิเตอร์</Link>
         </li>
         <li>
-          <Link to="/select-building/meter-record">จดมิเตอร์</Link>
+          <Link to={`/all-invoice/list/${props.dormId}`}>ใบแจ้งหนี้</Link>
         </li>
         <li>
           <Link to="/">เรื่องร้องเรียน</Link>
@@ -49,9 +53,43 @@ const AdminNav = (props) => {
         <li>
           <Link to="/rent/history">ประวัติการเช่าพัก</Link>
         </li>
-        <li>
+
+        <NavDropdown title="วิชัย ใจดี" id="navbarDropdown">
+          <NavDropdown.Item href="#action3">ข้อมูลส่วนตัว</NavDropdown.Item>
+
+          <NavDropdown.Item onClick={logout}>ออกจากระบบ</NavDropdown.Item>
+        </NavDropdown>
+        <i className="far fa-user-circle"></i>
+        {/* <li>
           <Button onClick={logout}>ออกจากระบบ</Button>
-        </li>
+        </li> */}
+        {/* <li className="nav-item dropdown">
+          <a
+            className="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdownMenuLink"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <i className="far fa-user-circle"></i> มัทยา
+          </a>
+          <ul
+            className="dropdown-menu"
+            aria-labelledby="navbarDropdownMenuLink"
+          >
+            <li>
+              <a className="dropdown-item" href="#">
+                ข้อมูลส่วนตัว
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" onClick={logout}>
+                ออกจากระบบ
+              </a>
+            </li>
+          </ul>
+        </li> */}
       </ul>
     </nav>
   );
