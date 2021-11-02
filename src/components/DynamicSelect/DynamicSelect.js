2f03386ad86814c7bac0db9b4a4d5513c90b1650
Donnaya <donnayawang@gmail.com>
2021-11-02 16:25:09 +0700

    [ADD] Dynamic dropdown for filter

---
+++ src/components/DynamicSelect/DynamicSelect.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import Form
+
+// const DynamicSelect = () => {
+//   // สร้างฟังกชันแล้ว ก็ loop
+//   // {value:1,label:'ชั้น 1'} ==> for => push ไปใน array ที่สร้างไว้ => array.push({value:i,label:'ชั้น ${i}'} )
+//   //ได้ array แล้วก็เอามา map
+//   return (
+//     <>
+//       <Form.Select aria-label="Default select example">
+//         {/* <option>Open this select menu</option>
+//         <option value="1">One</option>
+//         <option value="2">Two</option>
+//         <option value="3">Three</option> */}
+//         {/* map ค่า จาก API */}
+//       </Form.Select>
+//     </>
+//   );
+// };
+
+// export default DynamicSelect;
