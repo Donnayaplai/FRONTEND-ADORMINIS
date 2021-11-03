2925db450f118a0db74381cac09a13475a6ded09
nattacha <natchaaer@gmail.com>
2021-11-03 20:21:01 +0700

    [EDIT] selectrole

--- src/components/RegisterLogin/SelectRole.js
+++ src/components/RegisterLogin/SelectRole.js
@@ -1,41 +1,37 @@
-import React from 'react';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
-import residentIcon from '../../assets/images/resident.png';
-import adminIcon from '../../assets/images/admin.png';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import residentIcon from "../../assets/images/resident.png";
+import adminIcon from "../../assets/images/admin.png";
+import { Link } from "react-router-dom";
+import "./RegisterLogin.css";
 const SelectRole = () => {
   return (
     <>
-      <h1>เลือกตำแหน่งของคุณ</h1>
-      <Container>
+      <h1 id="topic"> เลือกตำแหน่งของคุณ</h1>
+      <Container className="mx-auto">
         <Row>
           <Col>
-            <Card style={{ maxWidth: '18rem' }} className="mx-auto">
+            <Card style={{ maxWidth: "18rem" }} className="mx-auto">
               <Card.Img variant="top" src={adminIcon} />
               <Card.Body>
                 <Card.Title></Card.Title>
                 <Card.Text>
-                  <div className="d-flex justify-content-center">
-                    <Link to="/admin/register">
-                      <Button id="btn-save">ผู้จัดการหอพัก</Button>
-                    </Link>
-                  </div>
+                  <Link to="/admin/register">
+                    <Button id="btn-selectrole">ผู้จัดการหอพัก</Button>
+                  </Link>
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
           <Col>
-            <Card style={{ maxWidth: '18rem' }}>
+            <Card style={{ maxWidth: "18rem" }}>
               <Card.Img variant="top" src={residentIcon} />
               <Card.Body>
                 <Card.Title></Card.Title>
                 <Card.Text>
-                  <div className="d-flex justify-content-center">
-                    <Link to="/resident/check-account">
-                      <Button id="btn-save">ผู้เช่า</Button>
-                    </Link>
-                  </div>
+                  <Link to="/resident/check-account">
+                    <Button id="btn-selectrole">ผู้เช่า</Button>
+                  </Link>
                 </Card.Text>
               </Card.Body>
             </Card>
