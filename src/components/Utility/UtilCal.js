9daba078639b21708b57f5765c0f1e936894059b
Donnaya <donnayawang@gmail.com>
2021-11-24 00:50:31 +0700

    [EDIT] Utility calculation

--- src/components/Utility/UtilCal.js
+++ src/components/Utility/UtilCal.js
@@ -3,126 +3,153 @@ import { Link } from 'react-router-dom';
 import { Container, Button, Row, Col, Form } from 'react-bootstrap';
 import axios from 'axios';
 import env from '../../env';
-import { useForm } from 'react-hook-form';
 import { withRouter } from 'react-router';
+import '../Dorm/Setting.css';
 
 const UtilCal = ({ filteredData, searchText, oldMeter, loading, ...props }) => {
-  const getOldMeterList = () => {
-    if (searchText === '') {
-      return oldMeter;
-    } else {
-      return filteredData;
-    }
-  };
-  const [error, setError] = useState();
-
-  console.log(oldMeter);
-
-  // POST คำนวนค่าไฟ-น้ำ สรุปผล และบันทึกลง db
-  // Path: http://localhost:3001/calculate/:dormID
-  // req params: dormID
-  // req body: arrayMeter[ { roomID, electricMeterNo, waterMeterNo } ]
-  // *electricMeterNo และ waterMeterNo ส่งเป็น null มาได้
-  // res: thisBillingCycle, summary[ { roomNo, electricUnit, electricPrice, waterUnit, waterPrice, totalPrice } ]
-
-  const { register, handleSubmit } = useForm();
-
-  //คำนวณค่าน้ำ ค่าไฟ
-  const onSubmit = async (data) => {
-    try {
-      await axios
-        .post(`${env.url}calculate/${props.dormId}`, {
-          arrayMeter: data,
-        })
-        .then(window.alert('คำนวณเสร็จสิ้น'));
-    } catch (err) {
-      if (err.response && err.response.data) {
-        setError(err.response.data.message);
-        window.alert(error);
-      }
-    }
-  };
+  const [waterMeterNo, setWaterMeterNo] = useState('');
+  const [electricMeterNo, setElectricityMeterNo] = useState('');
+  const [roomID, setSelectRoomID] = useState();
 
   if (loading) {
     return <h2 className="text-center text-dark fs-3 mt-5">Loading...</h2>;
   }
+
+  const onSubmit = async (e) => {
+    try {
+      console.log(roomID, electricMeterNo, waterMeterNo);
+      e.preventDefault();
+      await axios
+        .post(
+          `${env.url}calculate/${props.dormId}/${props.match.params.buildingId}`,
+          {
+            roomID,
+            electricMeterNo,
+            waterMeterNo,
+          }
+        )
+        .then(window.alert('คำนวณเสร็จสิ้น'));
+    } catch (err) {
+      if (err.response && err.response.data) {
+        console.log(err);
+      }
+    }
+  };
+
+  console.log(props.match.params.buildingId);
 
   return (
     <>
       {oldMeter.length === 0 ? (
         <h3 className="text-danger fw-bold text-center mt-5">ไม่พบข้อมูล</h3>
       ) : (
-        <Form onSubmit={handleSubmit(onSubmit)}>
-          <Container className="w-75">
+        <Form onSubmit={onSubmit}>
+          <>
             {oldMeter?.arrayRoomWithMeter?.map((meter) => (
               <Container
                 className="px-3 py-3 rounded mb-3 mt-3"
                 style={{ backgroundColor: '#EAE7E2' }}
                 key={meter.roomNo}
               >
-                <h6>ค่าน้ำ</h6>
-                <Row className="mb-3">
-                  <Col>
-                    <Form.Group>
-                      <Form.Label>เลขมิเตอร์ก่อนหน้า</Form.Label>
-                      <Form.Control
-                        type="text"
-                        disabled
-                        value={meter.oldWaterMeterNo}
-                      />
-                    </Form.Group>
-                  </Col>
-                  <Col>
-                    <Form.Group>
-                      <Form.Label>เลขมิเตอร์ปัจจุบัน</Form.Label>
-                      <Form.Control
-                        type="text"
-                        name="waterMeterNo"
-                        {...register('waterMeterNo')}
-                      />
-                    </Form.Group>
-                  </Col>
-                </Row>
-                <h6>ค่าไฟ</h6>
-                <Row className="mb-3">
-                  <Col>
-                    <Form.Group>
-                      <Form.Label>เลขมิเตอร์ก่อนหน้า</Form.Label>
-                      <Form.Control
-                        type="text"
-                        disabled
-                        value={meter.oldElectricMeterNo}
-                      />
-                    </Form.Group>
-                  </Col>
-                  <Col>
-                    <Form.Group>
-                      <Form.Label>เลขมิเตอร์ปัจจุบัน</Form.Label>
-                      <Form.Control
-                        type="text"
-                        name="electricMeterNo"
-                        {...register('electricMeterNo')}
-                      />
-                    </Form.Group>
-                  </Col>
-                </Row>
+                <h5 className="mb-3 fw-bold">ห้อง {meter.roomNo}</h5>
+                <Container className="px-5">
+                  <h6 className="fw-bold">ค่าน้ำ</h6>
+                  <Row className="mb-3">
+                    <Col>
+                      <Form.Group>
+                        <Form.Label>เลขมิเตอร์ก่อนหน้า</Form.Label>
+                        <Form.Control
+                          type="number"
+                          disabled
+                          defaultValue={meter.oldWaterMeterNo}
+                        />
+                      </Form.Group>
+                    </Col>
+                    <Col>
+                      <Form.Group>
+                        <Form.Label>เลขมิเตอร์ปัจจุบัน</Form.Label>
+                        <Form.Control
+                          type="number"
+                          min="0"
+                          max="9999"
+                          defaultValue={meter.newWaterMeterNo}
+                          name="waterMeterNo"
+                          onChange={(e) => setWaterMeterNo(e.target.value)}
+                        />
+                      </Form.Group>
+                    </Col>
+                  </Row>
+                  <h6 className="fw-bold">ค่าไฟ</h6>
+                  <Row className="mb-3">
+                    <Col>
+                      <Form.Group>
+                        <Form.Label>เลขมิเตอร์ก่อนหน้า</Form.Label>
+                        <Form.Control
+                          type="number"
+                          name="oldElectricMeterNo"
+                          disabled
+                          defaultValue={meter.oldElectricMeterNo}
+                        />
+                      </Form.Group>
+                    </Col>
+                    <Col>
+                      <Form.Group>
+                        <Form.Label>เลขมิเตอร์ปัจจุบัน</Form.Label>
+                        <Form.Control
+                          type="number"
+                          min="0"
+                          max="9999"
+                          name="electricMeterNo"
+                          onChange={(e) =>
+                            setElectricityMeterNo(e.target.value)
+                          }
+                          defaultValue={meter.newElectricMeterNo}
+                          // onChange={(e) =>
+                          //   onChangeElectricMeter(meter.roomID, e)
+                          // }
+                        />
+                      </Form.Group>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <Button
+                        variant="secondary"
+                        onClick={() => {
+                          setSelectRoomID(meter.roomID);
+                        }}
+                        type="submit"
+                        style={{ float: 'right' }}
+                      >
+                        บันทึก
+                      </Button>
+                    </Col>
+                  </Row>
+                </Container>
               </Container>
             ))}
             <Row className="mt-3">
               <Col>
-                <Link
+                {/* <Link
                   to={`/select-building/meter-record/${props.location.state.buildingId}`}
-                >
-                  <Button id="btn-back">ย้อนกลับ</Button>
-                </Link>
+                > */}
+                <Button id="btn-cancel">ย้อนกลับ</Button>
+                {/* </Link> */}
               </Col>
               <Col>
-                <Button id="btn-add" type="submit">
-                  คำนวณทั้งหมด
-                </Button>
+                <Link
+                  to={{
+                    pathname: `/utility-summary/${props.match.params.buildingId}`,
+                    state: { buildingId: props.match.params.buildingId },
+                  }}
+                >
+                  <Button id="btn-next" style={{ float: 'right' }}>
+                    สรุปผล
+                  </Button>
+                </Link>
               </Col>
             </Row>
-          </Container>
+          </>
         </Form>
       )}
     </>
