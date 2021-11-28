cf45c3755c17ddf6d94eb91dd31c7498530dae5e
Donnaya <donnayawang@gmail.com>
2021-11-28 09:15:01 +0700

    [EDIT] Resident navbar

--- src/components/Navbar/ResidentNav.js
+++ src/components/Navbar/ResidentNav.js
@@ -1,92 +1,64 @@
-import React from "react";
-import logo from "../../assets/images/building-nav.png";
-import { Link } from "react-router-dom";
-import { Button, Nav, Navbar, NavDropdown } from "react-bootstrap";
-import { useHistory } from "react-router";
-import { FiUser } from "react-icons/fi";
-import "./AdminNavBar.css";
+import React from 'react';
+import logo from '../../assets/building-nav.png';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import { useHistory } from 'react-router';
+import { FiUser } from 'react-icons/fi';
+import { MdLogout } from 'react-icons/md';
+import './AdminNav.css';
 
 const ResidentNav = (props) => {
   const history = useHistory();
   const logout = () => {
-    localStorage.removeItem("authorization");
+    localStorage.removeItem('authorization');
     props.setRoleId(null);
     props.setDormId(null);
     props.setRentId(null);
     props.setUserId(null);
     props.setUserFname(null);
     props.setUserLname(null);
-
-    history.push("/login");
+    history.push('/login');
   };
   return (
-    <div className="App">
-      <Navbar id="navbar1" sticky="top" expand="sm">
-        <Nav.Link href={`/admin/home`} id="title1">
-          ADORMINIS
-        </Nav.Link>
-
-        <Navbar.Toggle className="coloring" />
-        <Navbar.Collapse>
-          <Nav>
-            <Nav.Link href="/resident/dorm-info" id="title2">
-              ข้อมูลหอพัก
-            </Nav.Link>
-            <Nav.Link href="/resident/all-bill" id="title2">
-              ใบแจ้งหนี้
-            </Nav.Link>
-            <Nav.Link href={`/resident/complain-request`} id="title2">
-              แจ้งปัญหา
-            </Nav.Link>
-
-            <NavDropdown id="title2" title="โปรไฟล์">
-              {props.userFname}&nbsp;{props.userLname} <FiUser />
-              <NavDropdown.Item href="/resident/profile">
-                ข้อมูลส่วนตัว
-              </NavDropdown.Item>
-              <NavDropdown.Item onClick={logout}>ออกจากระบบ</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
-
-    // <nav>
-    //   <div className="logo">
-    //     <h2 className="title">
-    //       <Link
-    //         to="/resident/home"
-    //         style={{ textDecoration: "none", color: "#fff" }}
-    //       >
-    //         adorminis <img src={logo} alt="ADORMINIS-ICON" />
-    //       </Link>
-    //     </h2>
-    //   </div>
-    //   <input type="checkbox" id="click" style={{ display: "none" }} />
-    //   <label htmlFor="click" className="menu-btn">
-    //     <i className="fas fa-bars"></i>
-    //   </label>
-    //   <ul>
-    //     <li>
-    //       <Link to={`/resident/dorm-info`}>ข้อมูลหอพัก </Link>
-    //     </li>
-    //     <li>
-    //       <Link to="/resident/all-bill">ใบแจ้งหนี้</Link>
-    //     </li>
-    //     <li>
-    //       <Link to={`/resident/complain-request`}>แจ้งปัญหา</Link>
-    //     </li>
-    //     <li>
-    //       <Link to="/resident/profile">ข้อมูลส่วนตัว</Link>
-    //     </li>
-    //     <li style={{ color: "#fff" }}>
-    //       {props.userFname}&nbsp;{props.userLname} <FiUser />
-    //     </li>
-    //     <li>
-    //       <Button onClick={logout}>ออกจากระบบ</Button>
-    //     </li>
-    //   </ul>
-    // </nav>
+    <nav>
+      <div className="logo">
+        <h2 className="title">
+          <Link to="/resident/home">
+            adorminis <img src={logo} alt="ADORMINIS-ICON" />
+          </Link>
+        </h2>
+      </div>
+      <input type="checkbox" id="click" style={{ display: 'none' }} />
+      <label htmlFor="click" className="menu-btn">
+        <i className="fas fa-bars"></i>
+      </label>
+      <ul>
+        <li>
+          <Link to={`/resident/dorm-info`}>ข้อมูลหอพัก </Link>
+        </li>
+        <li>
+          <Link to="/resident/all-bill">ใบแจ้งหนี้</Link>
+        </li>
+        <li>
+          <Link to={`/resident/complain-request`}>แจ้งปัญหา</Link>
+        </li>
+        <li>
+          <Link to="/resident/profile">ข้อมูลส่วนตัว</Link>
+        </li>
+        <li>
+          <span>
+            "{props.userFname}&nbsp;{props.userLname}"
+          </span>
+          &nbsp;
+          <FiUser style={{ color: '#97d9fb', fontSize: '1.2em' }} />"
+        </li>
+        <li>
+          <Button id="logout-button" size="md" onClick={logout}>
+            ออกจากระบบ <MdLogout />
+          </Button>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
