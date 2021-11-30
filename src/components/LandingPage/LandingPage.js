b5200ac1675f035636a35b55f9b4ba802fe2b32b
Donnaya <donnayawang@gmail.com>
2021-11-30 10:25:07 +0700

    [EDIT] Landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Button } from 'react-bootstrap';
-import Logo from './home.png';
 import { MdLogin, MdManageAccounts } from 'react-icons/md';
+import Logo from './home.png';
 import './LandingPage.css';
 
 const LandingPage = () => {
@@ -29,9 +29,8 @@ const LandingPage = () => {
             </Col>
           </Row>
         </div>
-
         <div className="image">
-          <img src={Logo} alt="Home img" />
+          <img src={Logo} alt="Home" />
         </div>
       </section>
     </>
@@ -39,40 +38,3 @@ const LandingPage = () => {
 };
 
 export default LandingPage;
-
-// <header className="header">
-//         <h1>ร่วมรับประสบการณ์ดี ๆ กับ ADORMINIS วันนี้!</h1>
-//         <div className="d-grid gap-2 col-4 mx-auto">
-//           <Link to={'/role-selection'}>
-//             <button type="button" className="btn-header">
-//               สร้างบัญชีใหม่ &nbsp;<i className="fas fa-sign-in-alt fa-2x"></i>
-//             </button>
-//           </Link>
-//           <Link to={'/login'}>
-//             <button type="button" className="btn-header">
-//               ลงชื่อเข้าใช้ &nbsp;<i className="fas fa-sign-in-alt fa-2x"></i>
-//             </button>
-//           </Link>
-//         </div>
-//       </header>
-//       <div className="second-sec">
-//         <h2 className="text-center fw-bolder">คุณสมบัติเด่น ๆ</h2>
-//         <div className="container mt-5">
-//           <div className="row">
-//             <div className="col-lg-4 mb-3">
-//               <img src={Receipt} alt="Receipt" className="feature-img" />
-//             </div>
-//             <div className="col-lg-4  mb-3">
-//               <img
-//                 src={Utility}
-//                 alt="Utility calculation"
-//                 className="feature-img"
-//               />
-//             </div>
-
-//             <div className="col-lg-4  mb-3">
-//               <img src={RoomStatus} alt="Receipt" className="feature-img" />
-//             </div>
-//           </div>
-//         </div>
-//       </div>
