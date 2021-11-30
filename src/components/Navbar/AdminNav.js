9b63c43b0da9796a0cb78f57eb65ad617286bae4
Donnaya <donnayawang@gmail.com>
2021-11-30 10:21:00 +0700

    [EDIT] Admin navbar

--- src/components/Navbar/AdminNav.js
+++ src/components/Navbar/AdminNav.js
@@ -28,7 +28,6 @@ const AdminNav = (props) => {
   const createInvoiceBySendDormId = async () => {
     try {
       await axios.post(`${env.url}invoice/create/${props.dormId}`);
-      // console.log(props.dormId);
     } catch (err) {
       console.log(err);
     }
@@ -52,15 +51,6 @@ const AdminNav = (props) => {
         <li>
           <Link to="/about-dormitory/setting">เกี่ยวกับหอพัก</Link>
         </li>
-        {/* <li>
-          <Link to={'/room-setting'}>ตั้งค่าห้องพัก</Link>
-        </li> */}
-        {/* <li>
-          <Link to={'/create-room'}>สร้างห้อง</Link>
-        </li> */}
-        {/* <li>
-          <Link to={'/dorm-info'}>ข้อมูลหอพัก</Link>
-        </li> */}
         <li>
           <Link to={`/all-building`}>ตึกและห้องพัก</Link>
         </li>
@@ -81,9 +71,6 @@ const AdminNav = (props) => {
         <li>
           <Link to="/history">ประวัติการเช่าพัก</Link>
         </li>
-        {/* <li>
-          <Link to="/admin/profile">ข้อมูลส่วนตัว</Link>
-        </li> */}
         <li>
           <Link to="/admin/profile">
             <span>
@@ -97,75 +84,6 @@ const AdminNav = (props) => {
             ออกจากระบบ <MdLogout />
           </Button>
         </li>
-        {/* <li class="nav-item dropdown">
-          <a
-            class="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdown"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="true"
-          >
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li>
-              <a class="dropdown-item" href="/">
-                Action
-              </a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#">
-                Another action
-              </a>
-            </li>
-            <li>
-              <hr class="dropdown-divider" />
-            </li>
-            <li>
-              <a class="dropdown-item" href="#">
-                Something else here
-              </a>
-            </li>
-          </ul>
-        </li> */}
-
-        {/* <NavDropdown title="วิชัย ใจดี" id="navbarDropdown">
-          <NavDropdown.Item href="#action3">ข้อมูลส่วนตัว</NavDropdown.Item>
-
-          <NavDropdown.Item onClick={logout}>ออกจากระบบ</NavDropdown.Item>
-        </NavDropdown>
-        <i className="far fa-user-circle"></i> */}
-        {/* <li>
-          <Button onClick={logout}>ออกจากระบบ</Button>
-        </li> */}
-        {/* <li className="nav-item dropdown">
-          <a
-            className="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdownMenuLink"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <i className="far fa-user-circle"></i> มัทยา
-          </a>
-          <ul
-            className="dropdown-menu"
-            aria-labelledby="navbarDropdownMenuLink"
-          >
-            <li>
-              <a className="dropdown-item" href="#">
-                ข้อมูลส่วนตัว
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" onClick={logout}>
-                ออกจากระบบ
-              </a>
-            </li>
-          </ul>
-        </li> */}
       </ul>
     </nav>
   );
